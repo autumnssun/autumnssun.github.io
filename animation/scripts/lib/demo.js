6b746bed4ccb5c7bc4fcdc06ac0af7368081754c
Khoa Tran <autumns.sun@gmail.com>
2014-10-12 16:29:31 +1000

    add

---
+++ animation/scripts/lib/demo.js
@@ -0,0 +1,10 @@
+// function demo($scope) {
+    
+// };
+// $(function() {
+//   $('.nav a, .brand, a[data-target]').click(function(){
+//     $('html, body').animate({
+//       scrollTop: $($(this).attr('data-target')).offset().top-50
+//     }, 500)
+//   });
+// });
